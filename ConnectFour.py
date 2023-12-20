1730abde45233575e64ea55d6328d091f4f9ccca
Jacob Jensen <jakobeistheone@gmail.com>
2023-12-19 19:25:41 -0600

    Updated ConnectFour.py and Created a test file

--- ConnectFour.py
+++ ConnectFour.py
@@ -3,5 +3,17 @@ class ConnectFour:
     GAMEBOARD_WIDTH = 7
     GAMEBOARD_HEIGHT = 6
 
-    def __init__(self):
-        self.gameBoard:list[list[int]] = [[]]
+    def __init__(self, player1Name:str="", player2Name:str=""):
+        self.gameBoard:list[list[int]] = [[0 for _ in range(self.GAMEBOARD_WIDTH)] for _ in range(self.GAMEBOARD_HEIGHT)]
+        self._gameBoard:list[list[int]] = [[0 for _ in range(self.GAMEBOARD_WIDTH)] for _ in range(self.GAMEBOARD_HEIGHT)]
+        self.__gameBoard:list[list[int]] = [[0 for _ in range(self.GAMEBOARD_WIDTH)] for _ in range(self.GAMEBOARD_HEIGHT)]
+        self.player1Turn:bool = True
+        self.player1Name = player1Name
+        self.player2Name = player2Name
+
+    def get_gameboard(self)->list[list[int]]:
+        return self.__gameBoard
+
+
+connectFour = ConnectFour()
+quit()
---
+++ test.py
@@ -0,0 +1,6 @@
+from ConnectFour import *
+
+connectFour = ConnectFour()
+print(connectFour.gameBoard[0][0])
+print(connectFour._gameBoard[0][0])
+print(connectFour.__gameBoard[0][0])
