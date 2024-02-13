cb2312b66a852ad20d7a0a9ff44aab2099bacf5b
Jacob Jensen <jakobeistheone@gmail.com>
2024-02-13 11:04:04 -0600

    Update

--- minmax_connect_four_ai.py
+++ minmax_connect_four_ai.py
@@ -9,24 +9,26 @@ class MinimaxConnectFourAI:
     _NORMAL_DEPTH: int = 3
     _HARD_DEPTH: int = 5
     _MASTER_DEPTH: int = 7
-    
-    _HEAT_MAP = [[10, 20, 30, 40, 30, 20, 10],
-                 [10, 20, 30, 40, 30, 20, 10],
-                 [10, 20, 30, 40, 30, 20, 10],
-                 [10, 20, 30, 40, 30, 20, 10],
-                 [10, 20, 30, 40, 30, 20, 10],
-                 [10, 20, 30, 40, 30, 20, 10]]
-    
+
+    _HEAT_MAP = [
+        [10, 20, 30, 40, 30, 20, 10],
+        [10, 20, 30, 40, 30, 20, 10],
+        [10, 20, 30, 40, 30, 20, 10],
+        [10, 20, 30, 40, 30, 20, 10],
+        [10, 20, 30, 40, 30, 20, 10],
+        [10, 20, 30, 40, 30, 20, 10],
+    ]
+
     def __init__(self, name: str, piece_color: int, difficulty: str = "normal") -> None:
         self._name: str = name
         self._piece_color: int = piece_color
         self.difficulty: str = difficulty
         return
-    
+
     @property
     def name(self) -> str:
         return self._name
-    
+
     def _mininmax(self, game_state: ConnectFour, depth: int) -> float:
         alpha = -float("inf")
         beta = float("inf")
@@ -43,17 +45,27 @@ class MinimaxConnectFourAI:
             if best_value >= beta:
                 break
         return best_move
-    
-    def _minimax_rec(self, game_state: ConnectFour, depth: int, alpha: float, beta: float, maximizing_player: bool) -> int:
+
+    def _minimax_rec(
+        self,
+        game_state: ConnectFour,
+        depth: int,
+        alpha: float,
+        beta: float,
+        maximizing_player: bool,
+    ) -> int:
         if depth == 0 or game_state.winner is not None:
             return self._heuristic_function(game_state)
-        
+
         if maximizing_player:
             value = -float("inf")
             for move in game_state.legal_moves():
                 game_state_copy = copy.deepcopy(game_state)
                 game_state_copy.player_move(move)
-                value = max(value, self._minimax_rec(game_state_copy, depth - 1, alpha, beta, False))
+                value = max(
+                    value,
+                    self._minimax_rec(game_state_copy, depth - 1, alpha, beta, False),
+                )
                 alpha = max(alpha, value)
                 if value >= beta:
                     break
@@ -63,26 +75,31 @@ class MinimaxConnectFourAI:
             for move in game_state.legal_moves():
                 game_state_copy = copy.deepcopy(game_state)
                 game_state_copy.player_move(move)
-                value = min(value, self._minimax_rec(game_state_copy, depth - 1, alpha, beta, True))
+                value = min(
+                    value,
+                    self._minimax_rec(game_state_copy, depth - 1, alpha, beta, True),
+                )
                 beta = min(beta, value)
                 if value <= alpha:
                     break
             return value
-    
+
     def _heuristic_function(self, game_state: ConnectFour) -> float:
         if game_state.winner is not None and game_state.winner == self._name:
             return float("inf")
         elif game_state.winner is not None and game_state.winner != self._name:
             return -float("inf")
-        
+
         heuristic_value: float = 0
-        
+
         for row_idx, row in enumerate(game_state.game_board):
             for col_idx, space in enumerate(row):
-                heuristic_value += space * self._HEAT_MAP[row_idx][col_idx] * self._piece_color
-        
+                heuristic_value += (
+                    space * self._HEAT_MAP[row_idx][col_idx] * self._piece_color
+                )
+
         return heuristic_value
-    
+
     def move(self, game_state: ConnectFour) -> int:
         if self.difficulty == "easy":
             return self._mininmax(game_state, self._EASY_DEPTH)
